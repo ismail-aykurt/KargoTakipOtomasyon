15f1dc3fa6050b1e3265a70521cce3e103f64516
ismail <126421146+ismail-aykurt@users.noreply.github.com>
2025-01-17 19:33:02 +0300

    Add files via upload

---
+++ Untitled2.cpp
@@ -0,0 +1,223 @@
+#include <iostream>
+#include <fstream>
+#include <cstring>
+using namespace std;
+#include <locale>
+// Kargo Bilgileri için Struct Tanýmý
+struct Kargo {
+    int id;
+    char gonderici[50];
+    char alici[50];
+    char adres[100];
+    char durum[20];
+};
+
+// Fonksiyon Prototipleri
+void menu();
+void yeniKargoEkle();
+void kargoAra();
+void kargoGuncelle();
+void kargoSil();
+void kargolariListele();
+
+int main() {
+	setlocale(LC_ALL, "Turkish");
+    int secim;
+    do {
+        menu();
+        cout << "Bir iþlem seçin (1-6): ";
+        cin >> secim;
+        cin.ignore();
+        switch (secim) {
+            case 1: yeniKargoEkle(); break;
+            case 2: kargoAra(); break;
+            case 3: kargoGuncelle(); break;
+            case 4: kargoSil(); break;
+            case 5: kargolariListele(); break;
+            case 6: cout << "Programdan çýkýlýyor...\n"; break;
+            default: cout << "Geçersiz seçim! Tekrar deneyin.\n";
+        }
+    } while (secim != 6);
+    return 0;
+}
+
+// Menü Fonksiyonu
+void menu() {
+    cout << "\n=== Kargo Takip Otomasyonu ===\n";
+    cout << "1. Yeni Kargo Kaydý Ekle\n";
+    cout << "2. Kargo Bilgisi Ara\n";
+    cout << "3. Kargo Bilgisi Güncelle\n";
+    cout << "4. Kargo Kaydý Sil\n";
+    cout << "5. Tüm Kargolarý Listele\n";
+    cout << "6. Çýkýþ\n";
+}
+
+// Yeni Kargo Kaydý Ekleme Fonksiyonu
+void yeniKargoEkle() {
+    ofstream dosya("kargo.dat", ios::binary | ios::app);
+    Kargo yeniKargo;
+
+    cout << "Kargo ID girin: ";
+    cin >> yeniKargo.id;
+    cin.ignore();
+
+    cout << "Gönderici Adý: ";
+    cin.getline(yeniKargo.gonderici, 50);
+
+    cout << "Alýcý Adý: ";
+    cin.getline(yeniKargo.alici, 50);
+
+    cout << "Adres: ";
+    cin.getline(yeniKargo.adres, 100);
+
+    cout << "Durum: ";
+    cin.getline(yeniKargo.durum, 20);
+
+    dosya.write((char*)&yeniKargo, sizeof(Kargo));
+    dosya.close();
+    cout << "Kargo kaydý baþarýyla eklendi!\n";
+}
+
+// Kargo Bilgisi Arama Fonksiyonu
+void kargoAra() {
+	
+    ifstream dosya("kargo.dat", ios::binary);
+    if (!dosya) {
+        cout << "Dosya bulunamadý!\n";
+        return;
+    }
+
+    int arananID;
+    cout << "Aramak istediðiniz Kargo ID: ";
+    cout<<"//------------------------------------------------------------------------"<<endl;
+    cin >> arananID;
+
+    Kargo okunan;
+    bool bulundu = false;
+
+    while (dosya.read((char*)&okunan, sizeof(Kargo))) {
+        if (okunan.id == arananID) {
+            cout << "Kargo Bulundu!\n";
+            cout << "ID: " << okunan.id << "\n";
+            cout << "Gönderici: " << okunan.gonderici << "\n";
+            cout << "Alýcý: " << okunan.alici << "\n";
+            cout << "Adres: " << okunan.adres << "\n";
+            cout << "Durum: " << okunan.durum << "\n";
+            bulundu = true;
+            break;
+        }
+    }
+    dosya.close();
+
+    if (!bulundu) {
+        cout << "Kargo bulunamadý!\n";
+    }
+}
+
+// Kargo Bilgisi Güncelleme Fonksiyonu
+void kargoGuncelle() {
+	cout<<"//------------------------------------------------------------------------"<<endl;
+    fstream dosya("kargo.dat", ios::binary | ios::in | ios::out);
+    if (!dosya) {
+        cout << "Dosya bulunamadý!\n";
+        return;
+    }
+
+    int arananID;
+    cout << "Güncellemek istediðiniz Kargo ID: ";
+    cin >> arananID;
+    cin.ignore();
+
+    Kargo okunan;
+    bool bulundu = false;
+    int pozisyon;
+
+    while (dosya.read((char*)&okunan, sizeof(Kargo))) {
+        if (okunan.id == arananID) {
+            cout << "Kargo Bulundu! Güncelleme bilgilerini girin:\n";
+
+            cout << "Gönderici Adý: ";
+            cin.getline(okunan.gonderici, 50);
+
+            cout << "Alýcý Adý: ";
+            cin.getline(okunan.alici, 50);
+
+            cout << "Adres: ";
+            cin.getline(okunan.adres, 100);
+
+            cout << "Durum: ";
+            cin.getline(okunan.durum, 20);
+
+            pozisyon = dosya.tellg() - sizeof(Kargo);
+            dosya.seekp(pozisyon);
+            dosya.write((char*)&okunan, sizeof(Kargo));
+            bulundu = true;
+            cout << "Kargo bilgileri baþarýyla güncellendi!\n";
+            break;
+        }
+    }
+    dosya.close();
+
+    if (!bulundu) {
+        cout << "Kargo bulunamadý!\n";
+    }
+}
+
+// Kargo Kaydý Silme Fonksiyonu
+void kargoSil() {
+	cout<<"//----------------------------------------------------------------------"<<endl;
+    ifstream dosya("kargo.dat", ios::binary);
+    ofstream geciciDosya("gecici.dat", ios::binary);
+
+    if (!dosya) {
+        cout << "Dosya bulunamadý!\n";
+        return;
+    }
+
+    int silinecekID;
+    cout << "Silmek istediðiniz Kargo ID: ";
+    cin >> silinecekID;
+
+    Kargo okunan;
+    bool silindi = false;
+
+    while (dosya.read((char*)&okunan, sizeof(Kargo))) {
+        if (okunan.id != silinecekID) {
+            geciciDosya.write((char*)&okunan, sizeof(Kargo));
+        } else {
+            silindi = true;
+        }
+    }
+    dosya.close();
+    geciciDosya.close();
+
+    remove("kargo.dat");
+    rename("gecici.dat", "kargo.dat");
+
+    if (silindi) {
+        cout << "Kargo baþarýyla silindi!\n";
+    } else {
+        cout << "Kargo bulunamadý!\n";
+    }
+}
+
+// Tüm Kargolarý Listeleme Fonksiyonu
+void kargolariListele() {
+	cout<<"//----------------------------------------------------------------------------";
+
+    ifstream dosya("kargo.dat", ios::binary);
+    if (!dosya) {
+        cout << "Dosya bulunamadý!\n";
+        return;
+    }
+
+    Kargo okunan;
+    cout << "\n--- Tüm Kargolar ---\n";
+    while (dosya.read((char*)&okunan, sizeof(Kargo))) {
+        cout << "ID: " << okunan.id << ", Gönderici: " << okunan.gonderici
+             << ", Alýcý: " << okunan.alici << ", Adres: " << okunan.adres
+             << ", Durum: " << okunan.durum << "\n";
+    }
+    dosya.close();
+}
+
